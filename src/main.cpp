e221cd2122cda6a1d701f0825aaa74c217dab96f
Pontus Fransson <pontus.fransson92@gmail.com>
2020-11-15 22:12:35 +0100

    Scoped timer (#1)
    * Added ScopedTimer
    
    * Added support for using custom ostream for print out
    * Removed unnecessary constexpr. They don't make sense.

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,45 @@
 #include "Timer.h"
 
+#include <thread>
+#include <future>
+#include <sstream>
+
 using namespace std;
 
+void timeThisFunction() {
+	ScopedTimer<chrono::microseconds> timer("timeThisFunction timer");
+	this_thread::sleep_for(1ms);
+}
+
 int main() {
+	Timer<> timer("Timer overhead");
+	timer.printTime();
+
+	stringstream sstream;
+	{
+		ScopedTimer<> scopedTimer("Scoped timer", sstream);
+	}
+
 	Timer<chrono::microseconds> t1("Timer1");
 	Timer<> t2("Timer2");
 	Timer<chrono::milliseconds> t3;
-
-	cout << t3.getTime() << endl;
 	t2.printTime();
 	cout << t1 << endl;
+
+	cout << "String stream " << sstream.str();
+	{
+		ScopedTimer<chrono::microseconds> asyncTimer("Function timer");
+		timeThisFunction();
+	}
+	{
+		ScopedTimer<chrono::microseconds> asyncTimer("Async Timer");
+		auto future = async(launch::async, timeThisFunction);
+		future.wait();
+	}
+
+
+	auto t3Time = t3.getTime();
+	cout << t3Time << endl;
 
 	return 0;
 }
